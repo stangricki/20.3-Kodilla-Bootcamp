d36bfd361e65d38016b8fca84729fd65d9e11fe0
Filip Stangricki <stangricki@gmail.com>
2018-01-31 15:44:51 +0100

    Created Basic Data Structure

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+    </Provider>,
+    document.getElementById('root')
+);
